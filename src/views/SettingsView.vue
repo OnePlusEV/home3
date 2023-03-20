c81590d5b0658675ab31b3df7561f5cc2589e48a
dtatarinov <dtatarinov@bivgroup.ru>
2023-03-20 21:40:27 +0300

    add clock

---
+++ src/views/SettingsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+
+    <v-card title="General" class="ma-2">
+      <div class="d-flex flex-column">
+        <v-checkbox-btn
+            v-model=useLinks
+            label="Enable clock"
+        ></v-checkbox-btn>
+        <v-checkbox-btn
+            v-model=useLinks
+            label="Enable bookmarks"
+        ></v-checkbox-btn>
+      </div>
+    </v-card>
+
+  </v-container>
+</template>
